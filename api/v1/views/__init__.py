f81f031d4a571c9e3abaa69957c4bac326ce1063
kabakae <evanskabaka@gmail.com>
2024-05-26 12:36:51 -0700

    00

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,10 @@
+#!/usr/bin/python3
+"""
+This module initializes the blueprint for the views and imports the routes.
+"""
+
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-from api.v1.views.index import *
+from api.v1.views.index
