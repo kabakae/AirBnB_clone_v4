c1a22e70a1a5b2db675ffa0a7e9989dd7ed5e2ca
kabakae <evanskabaka@gmail.com>
2024-05-26 12:15:56 -0700

    000

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines the blueprint for the application views.
+"""
+
 from flask import Blueprint, jsonify
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
+    """
+    Returns the status of the application.
+
+    Returns:
+        JSON response with the status of the application.
+    """
     return jsonify({"status": "OK"})
