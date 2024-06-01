98ccca40d2ce1136661951c304e7f4a302868067
kabakae <evanskabaka@gmail.com>
2024-06-01 16:33:03 +0000

    00

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
-
 from flask import Flask, render_template
-import os
 import uuid
+import logging
 
 app = Flask(__name__)
 
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+
 @app.route('/0-hbnb/')
-def hbnb():
+def display_hbnb():
     cache_id = uuid.uuid4()
-    if os.path.exists('web_dynamic/templates/0-hbnb.html'):
-        template = '0-hbnb.html'
-    else:
-        template = '8-hbnb.html'
-    return render_template(template, cache_id=cache_id)
+    return render_template('0-hbnb.html', cache_id=cache_id)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    logging.info("Starting Flask application...")
+    try:
+        app.run(host="0.0.0.0", port=5000, debug=True)
+    except Exception as e:
+        logging.error("Error occurred: {}".format(e))
